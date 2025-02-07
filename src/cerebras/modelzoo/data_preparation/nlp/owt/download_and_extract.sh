b056d13febc7f6de133c547546518dfae42ce886
gadgilsanjeev <sanjeev@cerebras.net>
2025-02-06 16:29:39 -0800

    Release_2.4.2

--- src/cerebras/modelzoo/data_preparation/nlp/owt/download_and_extract.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install gdown
-gdown https://drive.google.com/uc?id=1EA5V0oetDCOke7afsktL_JDQ-ETtNOvx
-
-tar -xvf openwebtext.tar.xz
-cd openwebtext
-for f in *.xz; do mkdir ${f%.xz}; tar -Jvxf $f -C ${f%.xz}; rm $f; done
